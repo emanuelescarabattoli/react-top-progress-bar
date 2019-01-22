6665dba3532d226e117c85436dc42862a387b9bd
Emanuele Scarabattoli <em.scarabattoli@gmail.com>
2019-01-22 20:31:29 +0100

    Updates in docs

---
+++ docs/precache-manifest.f01815eafe66a364f66a6a33a6a18896.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ba61d4e78b1e331871f",
+    "url": "/static/js/main.6ba61d4e.chunk.js"
+  },
+  {
+    "revision": "150858e9bd420b1f59c1",
+    "url": "/static/js/1.150858e9.chunk.js"
+  },
+  {
+    "revision": "cd62e5938af55a3d0ee78b6b84b02035",
+    "url": "/index.html"
+  }
+];
