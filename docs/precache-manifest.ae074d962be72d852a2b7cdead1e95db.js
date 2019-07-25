5c33fd6dd17317df882038ecfb0aa34b14600c49
Emanuele Scarabattoli <em.scarabattoli@gmail.com>
2019-07-25 20:06:18 +0200

    Updated docs

---
+++ docs/precache-manifest.ae074d962be72d852a2b7cdead1e95db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d3ccdd2657d94ba99fe2eb293212c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b3993a54ee185b0f35ab",
+    "url": "./static/css/main.2d1bbc25.chunk.css"
+  },
+  {
+    "revision": "864feff6fb0577f0d83c",
+    "url": "./static/js/2.3369e408.chunk.js"
+  },
+  {
+    "revision": "b3993a54ee185b0f35ab",
+    "url": "./static/js/main.f0e57df1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
